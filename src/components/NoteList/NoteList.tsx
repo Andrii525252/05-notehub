0656510f17c608a67e69186954b0ed432c3e0faa
Andrii <simonandrij717@gmail.com>
2025-07-25 03:19:40 +0200

    fix tags

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -22,7 +22,7 @@ export default function NoteList({ notes }: NoteListProps) {
           <h2 className={css.title}>{note.title}</h2>
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
-            <span className={css.tag}>{note.tags}</span>
+            <span className={css.tag}>{note.tag}</span>
             <button
               className={css.button}
               onClick={() => mutation.mutate(note.id)}
